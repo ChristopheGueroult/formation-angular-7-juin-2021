6234189ac42c563415630f9f1cfcca6121d6ea5b
christophe <christophegueroult@gmail.com>
2021-06-08 11:45:19 +0200

    exemple attribute directive with NgClass

--- src/app/icons/icons.module.ts
+++ src/app/icons/icons.module.ts
@@ -1,12 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { IconDeleteComponent } from './components/icon-delete/icon-delete.component';
+import { IconEditComponent } from './components/icon-edit/icon-edit.component';
+import { IconCloseComponent } from './components/icon-close/icon-close.component';
+import { IconNavComponent } from './components/icon-nav/icon-nav.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [
+    IconDeleteComponent,
+    IconEditComponent,
+    IconCloseComponent,
+    IconNavComponent,
+  ],
+  imports: [CommonModule, FontAwesomeModule],
+  exports: [
+    IconDeleteComponent,
+    IconEditComponent,
+    IconCloseComponent,
+    IconNavComponent,
+  ],
 })
-export class IconsModule { }
+export class IconsModule {}
--- src/app/ui/components/ui/ui.component.ts
+++ src/app/ui/components/ui/ui.component.ts
@@ -3,13 +3,21 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-ui',
   templateUrl: './ui.component.html',
-  styleUrls: ['./ui.component.scss']
+  styleUrls: ['./ui.component.scss'],
 })
 export class UiComponent implements OnInit {
+  /**
+   * boolean used to open or close aside element
+   */
+  public open = true;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  /**
+   * fn to open or close aside menu
+   */
+  public toggle() {
+    this.open = !this.open;
   }
-
 }
