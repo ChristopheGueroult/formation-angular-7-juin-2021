58e2e4b0242fa614b066f144a5ef48be6483b699
christophe <christophegueroult@gmail.com>
2021-06-09 11:12:37 +0200

    @Input

--- src/app/orders/pages/page-list-orders/page-list-orders.component.ts
+++ src/app/orders/pages/page-list-orders/page-list-orders.component.ts
@@ -3,13 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-page-list-orders',
   templateUrl: './page-list-orders.component.html',
-  styleUrls: ['./page-list-orders.component.scss']
+  styleUrls: ['./page-list-orders.component.scss'],
 })
 export class PageListOrdersComponent implements OnInit {
+  public title = 'List Orders';
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  public changeTitle(): void {
+    this.title = 'New List Orders';
   }
-
 }
