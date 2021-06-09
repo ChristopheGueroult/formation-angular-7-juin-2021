21ee01d66c9701b6d61f40d76db154aacb90d3db
christophe <christophegueroult@gmail.com>
2021-06-09 12:33:49 +0200

    enum, interface, model

--- src/app/orders/pages/page-list-orders/page-list-orders.component.ts
+++ src/app/orders/pages/page-list-orders/page-list-orders.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { OrdersService } from '../../services/orders.service';
 
 @Component({
   selector: 'app-page-list-orders',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PageListOrdersComponent implements OnInit {
   public title = 'List Orders';
-  constructor() {}
+  constructor(private ordersService: OrdersService) {}
 
   ngOnInit(): void {}
   public changeTitle(): void {
---
+++ src/app/orders/services/orders.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class OrdersService {
+  // private collection
+  constructor() {
+    console.log('service called');
+  }
+
+  // getter collection
+
+  // setter collection
+
+  // change item state
+
+  // update item in collection
+
+  // delete item in collecion
+
+  // get item by id in collection
+}
