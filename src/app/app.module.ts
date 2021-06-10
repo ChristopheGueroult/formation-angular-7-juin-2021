34b546ada523c01e4336ec08d42defc3b761fcae
christophe <christophegueroult@gmail.com>
2021-06-10 12:09:36 +0200

    pipes api angular : async, currency, ... LOCAL ID an DEFAULT CURRENCY CODE

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,13 @@
 import { HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
+import { DEFAULT_CURRENCY_CODE, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
-
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, HttpClientModule, CoreModule, AppRoutingModule],
-  providers: [],
+  providers: [{ provide: DEFAULT_CURRENCY_CODE, useValue: 'EUR' }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/orders/pages/page-list-orders/page-list-orders.component.ts
+++ src/app/orders/pages/page-list-orders/page-list-orders.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
 import { Order } from 'src/app/core/models/order';
 import { OrdersService } from '../../services/orders.service';
 
@@ -7,9 +8,14 @@ import { OrdersService } from '../../services/orders.service';
   templateUrl: './page-list-orders.component.html',
   styleUrls: ['./page-list-orders.component.scss'],
 })
-export class PageListOrdersComponent implements OnInit {
+export class PageListOrdersComponent implements OnInit, OnDestroy {
+  private testObservable$ = new Observable((listXdeSubscribes) => {
+    listXdeSubscribes.next([{ name: 'chris' }, { name: 'Juju' }]);
+  });
+  private sub!: Subscription;
   public title = 'List Orders';
-  public collection!: Order[];
+  // public collection!: Order[];
+  public collection$!: Observable<Order[]>;
   public headers = [
     'Type',
     'Client',
@@ -20,15 +26,24 @@ export class PageListOrdersComponent implements OnInit {
     'State',
   ];
   constructor(private ordersService: OrdersService) {
-    this.ordersService.collection.subscribe((data) => {
-      this.collection = data;
-      console.log(data);
+    // this.ordersService.collection.subscribe((data) => {
+    //   this.collection = data;
+    //   console.log(data);
+    // });
+    this.collection$ = this.ordersService.collection;
+  }
+
+  ngOnInit(): void {
+    this.sub = this.testObservable$.subscribe((flux) => {
+      console.log(flux);
     });
   }
 
-  ngOnInit(): void {}
-
   public changeTitle(): void {
     this.title = 'New List Orders';
+  }
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
   }
 }
